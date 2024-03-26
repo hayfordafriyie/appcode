557fe0669cf617420ff14351daa97127f5bcd470
Hayford Afriyie <hayfordafriyie@protonmail.ch>
2024-03-26 12:03:14 +0000

    .npmrc file

---
+++ frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-gradient-to-r from-purple-500 via-indigo-500 to-blue-500 text-white">
+      <div className="text-center py-16">
+        <a href="#" className="text-white underline"></a>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
